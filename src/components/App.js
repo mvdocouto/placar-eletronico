23c0b346e28bfabdae86ad3c27dc14a867de71cf
Marcus Couto <marcus.couto@stormsec.com.br>
2018-02-27 18:42:54 -0300

    Passando os dados por referencia entre os componentes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import PlacarContainer from './PlacarContainer';
 
+const dados = {
+    partida: {
+        estadio: "Maracana/RJ",
+        data: "28/02/2017",
+        horario: "22:00"
+    },
+    casa: {
+        nome: "Flamengo"
+    },
+    visitante: {
+        nome: "River Plate"
+    }
+}
+
+
 export default class App extends React.Component {
     render(){
         return (
         <div>
-            <PlacarContainer />
+            <PlacarContainer partida={dados.partida}
+                casa={dados.casa}
+                visitante={dados.visitante}/>
         </div>
         )
     }
--- src/components/PlacarContainer.js
+++ src/components/PlacarContainer.js
@@ -3,19 +3,19 @@ import Time from './Time';
 import Partida from './Partida';
 
 export default class PlacarContainer extends React.Component {
-   
+       
     render(){
         return (
            <div>
                <div style={{ float: "left", width: "250px", marginRight: "10px", border: "1px solid #ccc", padding: "5px 5x", textAlign: "center", borderRadius: "5px"}}>
-                   <h2>Time da Casa</h2>
+                   <h3>Casa</h3>
                    <Time/>
                </div>
                <div style={{ float: "left", width: "150px", marginRight: "10px" , padding: "10px 10px", textAlign: "center"}}>
                    <Partida/>
                </div>
                <div style={{ float: "left", width: "250px", marginRight: "10px", border: "1px solid #ccc", padding: "5px 5px", textAlign: "center", borderRadius: "5px"}}>
-                    <h2>Time Visitante</h2>
+                    <h3>Visitante</h3>
                     <Time/>
                </div>
                <div style={{ clear: "both"}}></div>
